ca48e9a0fccd6390924d514dd46325eeb314a1fd
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-12 16:53:53 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(name)
+    @title = name
+  end
 
+  def turn_page=(turn_page)
+    @turn_page = turn_page
+  end
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+    @turn_page
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def cobble=(repair)
+    @cobble = repair
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+    @cobble
+  end
+
+
+end
