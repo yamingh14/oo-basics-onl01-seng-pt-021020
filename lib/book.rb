d69b7d46be6bcbbadf5ae7b40721db4abc4de8f2
Yamin Ghowrwal <yamin.gh@gmail.com>
2020-03-12 16:29:45 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,11 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(name)
+    @title = name
+  end
 
+  def turn_page
+
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,33 @@
-# Make your shoe class here!
+require 'pry'
+
+class Toy
+  attr_accessor :color, :brand, :price
+  @@all = []
+
+  def initialize(hue, maker, cost)
+    @color = hue
+    @brand = maker
+    @price = cost
+    @@all << self
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+      puts "Hi"
+    @name
+  end
+
+  def self.all
+      @@all
+  end
+binding.pry
+
+end
+
+superman = Toy.new("red", "Addidas", 200)
+hero = Toy.new("white", "sonic", 400)
+amir = Toy.new("purple", "Afghano", 45)
+pasoon = Toy.new("blue", "americano", 555)
